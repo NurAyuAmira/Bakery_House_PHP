a2a3b40f3600660d94785ab81cee8cdcd43d5239
yuz7_ <yuz9690@gmail.com>
2023-02-21 19:38:19 +0800

    Update message reply.php

--- message reply.php
+++ message reply.php
@@ -160,7 +160,7 @@
                 $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
                 $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
                 $mail->Username   = 'yuz9690@gmail.com';                     //SMTP username
-                $mail->Password   = 'heqsjassjwxabmjw';                               //SMTP password
+                $mail->Password   = '';                               //SMTP password
                 $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
                 $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
